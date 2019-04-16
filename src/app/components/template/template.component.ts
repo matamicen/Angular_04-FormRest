e42f83b2c11769c856f3e6502411c18bb810a2de
Matias Micenmacher <matamicen@gmail.com>
2019-04-16 16:57:21 -0300

    objeto Usuario y  [(ngModel)]

--- src/app/components/template/template.component.ts
+++ src/app/components/template/template.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { NgForm } from '@angular/forms';
+
 
 @Component({
   selector: 'app-template',
@@ -7,13 +9,23 @@ import { Component, OnInit } from '@angular/core';
 })
 export class TemplateComponent implements OnInit {
 
+  usuario:Object = {
+    nombre : "Jose",
+    apellido : "Meolans",
+    email : ""
+
+  }
   constructor() { }
 
   ngOnInit() {
   }
 
-  guardar() {
+  guardar(forma:NgForm) {
       console.log("guardo los cambios");
+      console.log("NgForm forma: ", forma);
+      console.log("forma value: ", forma.value);
+      console.log("usuario: ", this.usuario);
+      
       
 
   }
